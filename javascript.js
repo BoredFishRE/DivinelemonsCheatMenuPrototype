a310e6873e85e646d6af4fcd1e31852a68442656
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-04-21 09:17:16 -0700

    e

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function LevelPrompt() {
+  var Level = prompt("Please enter the level you would like to be- text is allowed:", "1e169");
+  if (Level == null || Level == ""){} 
+  else {
+    hack.instance.prodigy.player.getLevel = () => hack.instance.prodigy.player.data.level=Level;
+    Swal.fire('Success!', 'Your level has been changed!', 'success')
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+
+function AppearanceChanger() {
+   
+  Swal.fire('Success!', 'Your level has been changed!', 'success')
+
+  document.getElementById("demo").innerHTML = txt;
+}
